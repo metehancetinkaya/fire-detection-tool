5570c77ef3fc89cb2f50b9e422222005ed212153
Metehan Çetinkaya <50481841+metehancetinkaya@users.noreply.github.com>
2021-06-22 11:46:57 +0300

    first upload

---
+++ app.py
@@ -0,0 +1,77 @@
+#Import necessary libraries
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Create flask instance
+app = Flask(__name__)
+
+#Set Max size of file as 10MB.
+app.config['MAX_CONTENT_LENGTH'] = 10 * 2048 * 2048
+
+#Allow files with extension png, jpg and jpeg
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg']
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def init():
+    global graph
+    graph = tf.compat.v1.get_default_graph()
+
+# Function to load and prepare the image in right shape
+def read_image(filename):
+    # Load the image
+    img = load_img(filename, target_size=(196, 196))
+    # Convert the image to array
+    img = img_to_array(img)
+    # Reshape the image into a sample of 1 channel
+    img = img.reshape(1, 196, 196, 3)
+    # Prepare it as pixel data
+    img = img.astype('float32')
+    img = img / 255.0
+    return img
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route("/predict", methods = ['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        try:
+            if file and allowed_file(file.filename):
+                filename = file.filename
+                file_path = os.path.join('static/images', filename)
+                file.save(file_path)
+                img = read_image(file_path)
+                # Predict the class of an image
+
+                with graph.as_default():
+                    model1 = load_model('trainedModel.h5')
+                    class_prediction = model1.predict_classes(img)
+                    print(class_prediction)
+
+                #Map apparel category with the numerical class
+                if class_prediction[0] == 0:
+                  result = "Non Fire"
+                elif class_prediction[0] == 1:
+                  result = "Fire"
+                else:
+                  result = "Undefined"
+                return render_template('predict.html', result = result, user_image = file_path)
+        except Exception as e:
+            return "Unable to read the file. Please check if the file extension is correct."
+
+    return render_template('predict.html')
+
+if __name__ == "__main__":
+    init()
+    app.run()
