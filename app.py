3727f32868144d75aaa8dd1231d0d9837e4ef3c5
Metehan Çetinkaya <50481841+metehancetinkaya@users.noreply.github.com>
2021-06-24 19:51:59 +0300

    about screen

--- app.py
+++ app.py
@@ -42,6 +42,10 @@ def read_image(filename):
 def home():
     return render_template('home.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 @app.route("/predict", methods = ['GET','POST'])
 def predict():
     if request.method == 'POST':
